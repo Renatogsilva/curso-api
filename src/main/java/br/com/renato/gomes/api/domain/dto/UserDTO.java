630ab008cc94aef3d85442475eb67cfdb99e4ffb
renato gomes <silvarenato180@gmail.com>
2024-04-15 07:50:59 -0300

    Adicionando classe UserDTO

---
+++ src/main/java/br/com/renato/gomes/api/domain/dto/UserDTO.java
@@ -0,0 +1,18 @@
+package br.com.renato.gomes.api.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+}
