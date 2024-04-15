72900f730e00bb8bd95b2610df0a004503fd60c0
renato gomes <silvarenato180@gmail.com>
2024-04-15 07:56:19 -0300

    Retornando DTO para a controller

--- src/main/java/br/com/renato/gomes/api/controller/UserController.java
+++ src/main/java/br/com/renato/gomes/api/controller/UserController.java
@@ -1,7 +1,9 @@
 package br.com.renato.gomes.api.controller;
 
 import br.com.renato.gomes.api.domain.User;
+import br.com.renato.gomes.api.domain.dto.UserDTO;
 import br.com.renato.gomes.api.service.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,13 +15,16 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private ModelMapper mapper;
+
     @PostMapping
     public ResponseEntity<User> create(@RequestBody User user){
         return ResponseEntity.ok().body(userService.userCreate(user));
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Long id){
-        return ResponseEntity.ok().body(userService.findById(id));
+    public ResponseEntity<UserDTO> findById(@PathVariable Long id){
+        return ResponseEntity.ok().body(mapper.map(userService.findById(id), UserDTO.class));
     }
 }
