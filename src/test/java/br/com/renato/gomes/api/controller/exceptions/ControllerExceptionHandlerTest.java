f16d581a259abb656dee95d659998fe1e2c78951
renato gomes <silvarenato180@gmail.com>
2024-04-19 07:01:39 -0300

    Teste para manipulador de excecoes metodo objectNotFoundException

---
+++ src/test/java/br/com/renato/gomes/api/controller/exceptions/ControllerExceptionHandlerTest.java
@@ -0,0 +1,46 @@
+package br.com.renato.gomes.api.controller.exceptions;
+
+import br.com.renato.gomes.api.service.exceptions.ObjectNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ControllerExceptionHandlerTest {
+
+    public static final String REGISTRO_NAO_ENCONTRADO = "Registro não encontrado";
+    @InjectMocks
+    private ControllerExceptionHandler exceptionHandler;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void whenObjectNotFoundExceptionTheReturnAResponseEntity() {
+        ResponseEntity<StandardError> response = exceptionHandler
+                .objectNotFound(new ObjectNotFoundException(REGISTRO_NAO_ENCONTRADO), new MockHttpServletRequest());
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals(REGISTRO_NAO_ENCONTRADO, response.getBody().getError());
+        assertEquals(404, response.getBody().getStatus());
+    }
+
+    @Test
+    void dataIntegratyViolationException() {
+    }
+}
