856ce4a24418c500b572c554cc20e0ad8138b8e5
renato gomes <silvarenato180@gmail.com>
2024-04-19 07:11:08 -0300

    Teste para manipulador de excecoes metodo dataIntegraty

--- src/test/java/br/com/renato/gomes/api/controller/exceptions/ControllerExceptionHandlerTest.java
+++ src/test/java/br/com/renato/gomes/api/controller/exceptions/ControllerExceptionHandlerTest.java
@@ -1,5 +1,6 @@
 package br.com.renato.gomes.api.controller.exceptions;
 
+import br.com.renato.gomes.api.service.exceptions.DataIntegratyViolationException;
 import br.com.renato.gomes.api.service.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,6 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class ControllerExceptionHandlerTest {
 
     public static final String REGISTRO_NAO_ENCONTRADO = "Registro não encontrado";
+    public static final String E_MAIL_JA_CADASTRADO = "E-mail já cadastrado.";
     @InjectMocks
     private ControllerExceptionHandler exceptionHandler;
 
@@ -41,6 +43,16 @@ class ControllerExceptionHandlerTest {
     }
 
     @Test
-    void dataIntegratyViolationException() {
+    void dataIntegrityViolationException() {
+        ResponseEntity<StandardError> response = exceptionHandler
+                .dataIntegrityViolationException(new DataIntegratyViolationException(E_MAIL_JA_CADASTRADO), new MockHttpServletRequest());
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals(E_MAIL_JA_CADASTRADO, response.getBody().getError());
+        assertEquals(400, response.getBody().getStatus());
     }
 }
